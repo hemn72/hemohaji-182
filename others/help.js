0985e3aabc1ea1f437f20873d1da45eb82b8a644
Alinnaq1 <91326277+Alinnaq1@users.noreply.github.com>
2021-10-17 14:38:43 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -19,36 +19,17 @@ module.exports = {
     .setImage(`https://media.discordapp.net/attachments/848454202514079764/854642997894971392/image0.gif `)
     .setTitle(`AenGo Is One`)
     .setDescription(`
-*** | Prefix bot*** 
-***__+__***
-*** | User Commands***
- invite ・ support(supp)・ uptime ・ prefix
- ping ・ invites・avatar ・ se
- listEmoji ・user
- ***|Fun commnads***
- topinvites(tinvites)・ setnick
- lock ・ unlock ban ・ unban
- clear・kick ・channelinfo
- vkick・addrole ・ roleremove
- slowmode・invites ・createchannel
-deletechannel ・benner ・ count
-deafend ・ undeafend ・rolebc
-hightRole ・ rooms ・
-***| Gif Commands***
-boy gif ・ love gif・girl gif ・ sad gif・meme ・ 
-anime
-***| photo Commnads ***
-pboy ・ pgirl ・ panime 
-***| Game gif Commands***
-boom ・ hug  ・ rps ・ kiss ・ slap ・slots
-***| Game Commands***
-rps・slots・stone・paper・scissors・kill
-***| Music Commands***
-play ・ skip・lyrics
-nowplaying・skipto・ stop
-volume・nowplaying ・ resume
-shuffle ・ search ・remove
-queue ・ filter ・ radio・loob
+*** |General |***
+help, ping, prefix, avatar
+***  |   Music ***
+filter, loop, lyrics, np, play,
+queue, remove, shuffle, skip,
+skipto, stop, volume
+***Info***
+botinfo, serverinfo, userinfo,
+status, uptime
+ds |
+
 **Links**
 [SUPPORT](https://discord.gg/BCHan3ZxZr)       [INVITE](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot) [  website  ](https://calms-web-site.glitch.me/)
 `)
