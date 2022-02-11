62c5ba29deb842d15736cab103f62e244532a4da
hemn72 <92395917+hemn72@users.noreply.github.com>
2022-02-11 21:46:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('Nzg0MzAzNzU2OTI1NDY4NzEz.X8nVyw.eSrDPfzRnwffCXIT3UbiV_DQKTc');
+client.login('OTQxNzYyMjE3NzYwNjY5NzM3.YgaqaQ.u_sbb-CUAPuVIYJZmzxcROHRpT0');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
